d2999c16da332be810bbd12e7e0a3367da6bae9a
Francisco Herrera <FranHerreraG@users.noreply.github.com>
2018-08-01 15:06:02 +0200

    Tratamiento de datos

--- Tratamiento de los datos.R
+++ Tratamiento de los datos.R
@@ -1,11 +1,28 @@
 library(readr)
 library(dplyr)
+library(ggplot2)
+library(gridExtra)
+library(tabplot) #mirar cran tableplot(fifa18[,1:10], cex = 1.8)
+
+
 fifa18<- read_csv("data/FIFA18v2.csv")
 
-fifa18 %>% 
+c <-fifa18 %>% 
   group_by(Club) %>% 
   summarise(meanValue = sum(Value), maxValue = max(Value)) %>% 
   arrange(desc(meanValue))
+
+c <- as.data.frame(head(c,15))
+
+k2 <- ggplot(c,aes(x=c$"Club",y=c$"meanValue")) + 
+  geom_point()
+ss2 <- tableGrob(c)
+#Arrange them as you want with grid.arrange
+jpg(file=mypath)
+mytitle = paste("my title is", names[i])
+plot(x,y, main = mytitle)
+dev.off()
+grid.arrange(k2,ss2)
 
 
 fifa18 <- as.data.frame(fifa18[,!(colnames(fifa18) %in% c("Photo", "Flag", "Club Logo"))])
@@ -17,6 +34,7 @@ fifa18$Nationality=as.factor(fifa18$Nationality)
 fifa18$Club=as.factor(fifa18$Club)
 
 summary(fifa18)
+
 
 fifa18[fifa18$Nationality=="France",]
 
