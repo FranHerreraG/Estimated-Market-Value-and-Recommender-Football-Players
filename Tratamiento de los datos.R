78821818424b76df8dd9e408b8b55dad99137e4d
Francisco Herrera <37455252+FranHerreraG@users.noreply.github.com>
2018-11-12 18:15:16 +0100

    Recomendador

--- Tratamiento de los datos.R
+++ Tratamiento de los datos.R
@@ -1,29 +1,39 @@
-library(readr)
-library(dplyr)
+library(tidyverse)
 
-df <- read_delim("Data/Fichajes18.csv", ";", 
-                 escape_double = FALSE, locale = locale(encoding = "ISO-8859-1"), 
-                 trim_ws = TRUE)
+df2 <- read_delim("Data/df2.csv", ";", escape_double = FALSE, 
+                       locale = locale(encoding = "ISO-8859-1"), 
+                       trim_ws = TRUE)
 
-df$ultimoClub=as.factor(df$ultimoClub)
-df$nuevoClub=as.factor(df$nuevoClub)
-df$mercado=as.factor(df$mercado)
+Variables <- c("rs", "rw", "rf", "ram", "rcm", "rm", "rcb", "rb", "rwb", "st", "lw","cam",
+               "lm", "cdm", "cb", "lb", "lwb", "lf", "lcm", "ldm", "lcb", "gk")
+i=1
+for(i in 1:length(Variables)){
+Variables[i] =  paste("prefers_",Variables[i],sep = "")
+}
+Variables
 
-summary(df)
+#Cambiar Logical a Number
+converter <- function(var){
+  return(as.numeric(as.logical(var)))
+}
 
-ventas <- df %>% 
-  group_by(ultimoClub) %>% 
-  mutate(coste = coste/1000000) %>% 
-  summarise(media = mean(coste), mediana = median(coste),total = sum(coste),jugadores = n()) %>% 
-  arrange(desc(total))
+for(i in 1:length(Variables)){
+  n= which(colnames(df2)==Variables[i])
+  a=paste("df2$",Variables[i],sep="")
+  df2[,n] = converter(eval(parse( text=a )) )
+}
 
-compras <- df %>% 
-  group_by(nuevoClub) %>% 
-  mutate(coste = coste/1000000) %>% 
-  summarise(media = mean(coste), mediana = median(coste),total = sum(coste),jugadores = n()) %>% 
-  arrange(desc(total))
+KNN_Model = knn(df[])
 
-#Vemos que Equipos como Monaco y Roma son los que más han ingresado y Liverpool y Juventus los que más han gastado
+#EJEMPLO A REPPLICAR
+library(FNN)
+KNN_Model = knn(ma[-3,-5],ma[3,-5],ma[-3,4],k=5)
+attr(KNN_Model, "nn.index")
+ma
+
+ma = cbind(ma,seq(c(1,2,3,4,5,6)))
+ma[,4] = rep(c(T,F,T),2)
+ma
 
 
 
