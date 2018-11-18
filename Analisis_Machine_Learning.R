2fcf9fe6fba9f46804b78fe2fee25bfe2b397211
Francisco Herrera <FranHerreraG@users.noreply.github.com>
2018-11-18 20:48:04 +0100

    act all

--- Analisis_Machine_Learning.R
+++ Analisis_Machine_Learning.R
@@ -1,3 +1,7 @@
+list.of.packages <- c("tidyverse", "corrplot", "lubridat", "fmsb")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(tidyverse)
 library(corrplot)
 library(lubridate)
